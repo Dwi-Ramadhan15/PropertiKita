89a42bd59a3cc739a69647606ca10ae29e8e4044
Diahayusafitri15 <Dhyusftri@gmail.com>
2026-04-07 14:11:18 +0700

    kode backend 1 dan 2 fix

--- backend/utils/swagger.js
+++ backend/utils/swagger.js
@@ -2,24 +2,6 @@ const swaggerJsdoc = require('swagger-jsdoc');
 const swaggerUi = require('swagger-ui-express');
 
 const options = {
-<<<<<<< HEAD
-  definition: {
-    openapi: '3.0.0',
-    info: {
-      title: 'PropertiKita API Documentation',
-      version: '1.0.0',
-      description: 'Dokumentasi API untuk sistem manajemen properti "PropertiKita" (Backend 2)',
-    },
-    servers: [
-      {
-        url: 'http://localhost:5000',
-        description: 'Local server',
-      },
-    ],
-  },
-  // Path ke file yang berisi anotasi JSDoc (Routes)
-  apis: ['./routes/*.js'], 
-=======
     definition: {
         openapi: '3.0.0',
         info: {
@@ -33,16 +15,8 @@ const options = {
         }],
     },
     apis: ['./routes/*.js'],
->>>>>>> 6f07009e609101757d963ae2098fc4fb82869913
 };
 
 const specs = swaggerJsdoc(options);
 
-<<<<<<< HEAD
-module.exports = {
-  swaggerUi,
-  specs,
-};
-=======
 module.exports = { swaggerUi, specs };
->>>>>>> 6f07009e609101757d963ae2098fc4fb82869913
