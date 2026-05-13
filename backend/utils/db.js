f360a212057bc122cd9287cd885c52295b021687
Dwi_Ramadhan <dwir57017@gmail.com>
2026-05-13 18:36:59 +0700

    Perbaikan tampilan mode handphone/mobile

--- backend/utils/db.js
+++ backend/utils/db.js
@@ -1,17 +1,18 @@
 const { Pool } = require('pg');
 require('dotenv').config();
 
-// Cek apakah ada DATABASE_URL (berarti lagi di Vercel / Cloud)
 const poolConfig = process.env.DATABASE_URL ? {
     connectionString: process.env.DATABASE_URL,
-    ssl: { rejectUnauthorized: false } // Wajib untuk Neon
+    ssl: { rejectUnauthorized: false }
 } : {
-    // Kalau jalan di laptop lokal, pakai setingan bawaan ini
     user: process.env.DB_USER,
     host: process.env.DB_HOST,
     database: process.env.DB_NAME,
     password: process.env.DB_PASSWORD,
     port: process.env.DB_PORT,
+    ssl: {
+        rejectUnauthorized: false
+    }
 };
 
 const pool = new Pool(poolConfig);
