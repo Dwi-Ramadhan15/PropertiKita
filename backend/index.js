be8f26d29a860691bb70e580cb7124908fe0cba9
Dwi_Ramadhan <dwir57017@gmail.com>
2026-05-13 17:44:14 +0700

    Perbaikan tampilan mode handphone/mobile

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,8 @@ const app = express();
 const server = http.createServer(app);
 const allowedOrigins = [
     "http://localhost:5173",
-    "https://properti-kita-eight.vercel.app"
+    "https://properti-kita-eight.vercel.app",
+    "https://properti-kita-p0l475d35-dwi-ramadhan15s-projects.vercel.app/"
 ];
 
 const io = new Server(server, {
